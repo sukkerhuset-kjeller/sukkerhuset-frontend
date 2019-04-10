a32618b8d94881b489ea1ab0c0f23f255cc009be
Ole Kristian Aune <okaune@outlook.com>
2019-04-10 23:29:22 +0200

    Responsive table layout

--- src/modules/Table.js
+++ src/modules/Table.js
@@ -2,44 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 import MailLink from './MailLink';
 
-const StyledTable = styled.table`
-  width: 100%;
-  max-width: 100%;
-  margin-bottom: 20px;
-  background-color: transparent;
-  border-spacing: 0;
-  border-collapse: collapse;
+const StyledTable = styled.div`
+  overflow-x: auto;
 
-  tr:nth-child(odd) {
-    background-color: #f9f9f9;
-  }
+  table {
+    width: 100%;
+    margin-bottom: 20px;
+    background-color: transparent;
+    border-spacing: 0;
+    border-collapse: collapse;
 
-  td {
-    padding: 5px;
-    line-height: 1.42857143;
-    vertical-align: top;
-    border-top: 1px solid #ddd;
-    font-weight: 300;
-
-    @media (max-width: 600px) {
-      display: flex;
-      flex-flow: column nowrap;
-      border: none;
-      padding: 0;
-      &:first-child {
+    tr {
+      td {
+        padding: 5px;
+        line-height: 1.42857143;
+        vertical-align: top;
         border-top: 1px solid #ddd;
+        font-weight: 300;
+        white-space: nowrap;
       }
-      &:last-child {
-        border-bottom: 1px solid #ddd;
+
+      &:nth-child(odd) {
+        background-color: #f9f9f9;
       }
-    }
-  }
 
-  tr:first-child td {
-    font-weight: normal;
-
-    @media (max-width: 600px) {
-      display: none;
+      &:first-child {
+        td {
+          font-weight: normal;
+        }
+      }
     }
   }
 `;
@@ -47,17 +38,19 @@ const StyledTable = styled.table`
 const Table = ({ table }) => {
   return table ? (
     <StyledTable>
-      <tbody>
-        {table.rows.map((row, rindex) => (
-          <tr key={rindex}>
-            {row.cells.map((cell, cindex) => (
-              <td key={cindex}>
-                {cell.includes('mailto:') ? <MailLink link={cell} /> : cell}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
+      <table>
+        <tbody>
+          {table.rows.map((row, rindex) => (
+            <tr key={rindex}>
+              {row.cells.map((cell, cindex) => (
+                <td key={cindex}>
+                  {cell.includes('mailto:') ? <MailLink link={cell} /> : cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </StyledTable>
   ) : (
     <></>
